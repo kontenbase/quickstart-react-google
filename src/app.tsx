7d3b3ace858543dc7e91fc93636335f9f2291a6c
M Haidar Hanif <git@mhaidarhanif.com>
2022-10-26 20:40:45 +0700

    feat: use kontenbase sdk

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+import { kontenbase } from "./libs";
+
 export const App = () => {
-  return (
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const { data, error } = await kontenbase.service("todos").find();
+      if (data) {
+        setTodos(data as any);
+      } else if (error) {
+        console.error(error);
+      }
+    };
+
+    fetchTodos();
+  });
+
+  https: return (
     <div>
       <h1>Kontenbase + React + Google</h1>
+      {todos && <pre>{JSON.stringify(todos, null, 2)}</pre>}
     </div>
   );
 };
